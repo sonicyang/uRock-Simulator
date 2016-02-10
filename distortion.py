af49c700a7d4b4104140a72821bd5be458eee560
sonicyang <sonic.tw.tp@gmail.com>
2016-02-10 13:36:50 +0800

    Add distortion effect implemented by amplification

--- distortion.py
+++ distortion.py
@@ -1,11 +1,12 @@
 import simbase
 
-class distortion(simbase.simulation):
+class distortion_threshold(simbase.simulation):
     def __init__(self, n_sine):
-        super(distortion, self).__init__(n_sine)
+        super(distortion_threshold, self).__init__(n_sine)
 
-        self.name = "Distortion"
-        self.threshold = 0.5
+        self.name = "Distortion Threshold Method"
+        self.threshold = 0.05
+        return
 
     def process(self, sample):
         if abs(sample) > self.threshold:
@@ -13,7 +14,26 @@ class distortion(simbase.simulation):
         return sample
 
     def post_simulation(self):
-        hline1 = self.ax.axhline(0.5, color='r', ls='dashed', label='Threshold')
-        self.ax.axhline(-0.5, color='r', ls='dashed', label='Threshold')
+        hline1 = self.ax.axhline(self.threshold, color='r', ls='dashed', label='Threshold')
+        self.ax.axhline(-self.threshold, color='r', ls='dashed', label='Threshold')
 
-        self.legends = [hline1]
+        self.legends.append(hline1)
+        return
+
+class distortion_amplification(simbase.simulation):
+    def __init__(self, n_sine):
+        super(distortion_amplification, self).__init__(n_sine)
+
+        self.name = "Distortion Amplification Method"
+        self.gain = 10.0
+
+        return
+
+    def process(self, sample):
+        sample = sample * self.gain
+        if abs(sample) > 1:
+            sample = sample / abs(sample)
+        return sample
+
+    def post_simulation(self):
+        return
