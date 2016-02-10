59319a78858f8a4fe7766ea23836ad3fc2759a23
sonicyang <sonic.tw.tp@gmail.com>
2016-02-10 19:44:20 +0800

    Refind to separate Overdrive from distortion, supply sigmoid to clipping method

--- distortion.py
+++ distortion.py
@@ -1,48 +1,10 @@
 import simbase
 
-class distortion_threshold(simbase.simulation):
+class overdrive_sigmoid(simbase.simulation):
     def __init__(self, n_sine):
-        super(distortion_threshold, self).__init__(n_sine)
+        super(overdrive_sigmoid, self).__init__(n_sine)
 
-        self.name = "Distortion Threshold Method"
-        self.threshold = 0.05
-        return
-
-    def process(self, sample):
-        if abs(sample) > self.threshold:
-            sample = sample / abs(sample) * self.threshold
-        return sample
-
-    def post_simulation(self):
-        hline1 = self.ax.axhline(self.threshold, color='r', ls='dashed', label='Threshold')
-        self.ax.axhline(-self.threshold, color='r', ls='dashed', label='Threshold')
-
-        self.legends.append(hline1)
-        return
-
-class distortion_amplification(simbase.simulation):
-    def __init__(self, n_sine):
-        super(distortion_amplification, self).__init__(n_sine)
-
-        self.name = "Distortion Amplification Method"
-        self.gain = 10.0
-
-        return
-
-    def process(self, sample):
-        sample = sample * self.gain
-        if abs(sample) > 1:
-            sample = sample / abs(sample)
-        return sample
-
-    def post_simulation(self):
-        return
-
-class distortion_sigmoid(simbase.simulation):
-    def __init__(self, n_sine):
-        super(distortion_sigmoid, self).__init__(n_sine)
-
-        self.name = "Distortion Sigmoid Method"
+        self.name = "Overdrive Sigmoid Method"
         self.gain = 10
         self.sigmoid_function = self.fast_sigmoid
         return
@@ -57,3 +19,44 @@ class distortion_sigmoid(simbase.simulation):
 
     def fast_sigmoid(self, sample):
         return sample / (1 + abs(sample))
+
+class distortion_threshold(overdrive_sigmoid):
+    def __init__(self, n_sine):
+        super(distortion_threshold, self).__init__(n_sine)
+
+        self.name = "Distortion Threshold Method"
+        self.threshold = 0.05
+        return
+
+    def process(self, sample):
+        super(distortion_threshold, self).process(sample)
+        if abs(sample) > self.threshold:
+            sample = sample / abs(sample) * self.threshold
+        return sample
+
+    def post_simulation(self):
+        hline1 = self.ax.axhline(self.threshold, color='r', ls='dashed', label='Threshold')
+        self.ax.axhline(-self.threshold, color='r', ls='dashed', label='Threshold')
+
+        self.legends.append(hline1)
+        return
+
+class distortion_amplification(overdrive_sigmoid):
+    def __init__(self, n_sine):
+        super(distortion_amplification, self).__init__(n_sine)
+
+        self.name = "Distortion Amplification Method"
+        self.gain = 10.0
+
+        return
+
+    def process(self, sample):
+        super(distortion_amplification, self).process(sample)
+        sample = sample * self.gain
+        if abs(sample) > 1:
+            sample = sample / abs(sample)
+        return sample
+
+    def post_simulation(self):
+        return
+
--- simulate.py
+++ simulate.py
@@ -1,12 +1,12 @@
 import distortion
 
+overdrive = distortion.overdrive_sigmoid(2)
 distor1 = distortion.distortion_threshold(2)
 distor2 = distortion.distortion_amplification(2)
-distor3 = distortion.distortion_sigmoid(2)
 
+overdrive .simulate()
 distor1.simulate()
 distor2.simulate()
-distor3.simulate()
 
 try:
     input("Press Enter to continue...")
